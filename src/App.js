8eb7be14cb9edc3cd106a8257f72d784e6880993
Oktay <01210102053@ogr.inonu.edu.tr>
2023-02-08 15:09:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// scss
+import "scss/main.scss";
+// React Router
+import {Routes, Route} from "react-router-dom";
+// Layout components
+import Layout from "layout/StarWars";
+// Pages components
+import Home from "pages/Home";
+import Starship from "pages/Starship";
+import Planet from "pages/Planet";
+import People from "pages/People";
+import Vehicle from "pages/Vehicle";
+import NoMatch from "pages/NoMatch";
+
+// Pages Details components
+import StarshipDetail from "pages/StarshipDetail";
+import PlanetDetail from "pages/PlanetDetail";
+import PeopleDetail from "pages/PeopleDetail";
+import VehicleDetail from "pages/VehicleDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="starships">
+          <Route index element={<Starship />} />
+          <Route path=":id" element={<StarshipDetail />} />
+        </Route>
+        <Route path="planets">
+          <Route index element={<Planet />} />
+          <Route path=":id" element={<PlanetDetail />} />
+        </Route>
+        <Route path="peoples">
+          <Route index element={<People />} />
+          <Route path=":id" element={<PeopleDetail />} />
+        </Route>
+        <Route path="vehicles">
+          <Route index element={<Vehicle />} />
+          <Route path=":id" element={<VehicleDetail />} />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
